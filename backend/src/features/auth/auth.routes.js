becaa26396f7c880cc45a7ed90d3260c6208df04
Chenevoix Clemence <cchenevoix@gmail.com>
2026-09-14 09:34:00 +0200

    FEAT(auth): init auth route

---
+++ backend/src/features/auth/auth.routes.js
@@ -0,0 +1,11 @@
+// require
+const express = require('express');
+const router = express.Router();
+
+// implementation of the routes logic for the authentification
+
+// login to an account
+router.post('/login')
+
+// export
+module.exports = router
