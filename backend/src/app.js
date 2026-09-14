4de983e63ede38211c6d723a855a551ec721370f
Chenevoix Clemence <cchenevoix@gmail.com>
2026-09-14 10:23:46 +0200

    FEAT(routes): link all the routes to the api route

---
+++ backend/src/app.js
@@ -0,0 +1,25 @@
+// Express app entry point
+
+const express = require('express');
+const dotenv = require('dotenv');
+
+// Routes
+const authRoutes = require('./features/auth/auth.routes.js');
+const adminRoutes = require('./features/admin/admin.routes.js');
+const catalogRoutes = require('./features/catalog/catalog.routes.js');
+
+dotenv.config();
+
+const app = express();
+
+// Auth routes
+app.use('/api/auth', authRoutes);
+
+// Admin routes
+app.use('/api/admin', adminRoutes);
+
+// Catalog routes
+app.use('/api/catalog', catalogRoutes);
+
+// export
+module.exports = app;
