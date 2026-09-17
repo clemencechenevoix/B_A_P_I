84e7a124d8afccfc5f47958b51eba76934915007
clemence chenevoix <cchenevoix@gmail.com>
2026-09-17 15:18:25 +0200

    FEAT(admin): init delete routes for the locations and the familys

--- backend/src/features/admin/admin.routes.js
+++ backend/src/features/admin/admin.routes.js
@@ -4,14 +4,20 @@ const router = express.Router();
 
 // implementation of the routes logic for the admin
 
+// show the information about all the family
+router.get('/family')
+
 // create a new family for the flower
 router.post('/family')
 
 // update the id family
 router.put('/family/:id')
 
-// show the information about all the family
-router.get('/family')
+// delete the family id
+router.delete('/family/:id')
+
+// show the information about all the location
+router.get('/location')
 
 // create a new location for the flower
 router.post('/location')
@@ -19,8 +25,8 @@ router.post('/location')
 // update the id location
 router.put('/location/:id')
 
-// show the information about all the location
-router.get('/location')
+// delete the location id
+router.delete('/location/:id')
 
 // export
 module.exports = router
