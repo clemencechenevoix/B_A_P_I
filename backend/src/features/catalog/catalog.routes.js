ece73f1fa35dfdf69ee93455e736d84fea2f96b1
clemence chenevoix <cchenevoix@gmail.com>
2026-09-24 14:03:21 +0200

    FEAT(catalog): add the role verification on the management tool (CRUD) of the flowers

--- backend/src/features/catalog/catalog.routes.js
+++ backend/src/features/catalog/catalog.routes.js
@@ -5,22 +5,25 @@ const router = express.Router();
 const { showFlower, showFlowerId, createFlower, updateFlower, deleteFlower} = require('./catalog.controller')
 const { validationId, validationPage, validationInformationUpdateFLower, validationInformationCreateFlower } = require('./catalog.validation')
 
+const {authMiddleware} = require("../../security/middleware/auth.middleware")
+const {rolesMiddleware} = require("../../security/middleware/roles.middleware")
+
 // implementation of the routes logic for the catalog
 
 // show the information of the flower for the catalog
 router.get('/', validationPage, showFlower)
 
 // create a flower for the catalog (only usable for the Admin)
-router.post('/items', validationInformationCreateFlower, createFlower)
+router.post('/items', authMiddleware, rolesMiddleware('admin'), validationInformationCreateFlower, createFlower)
 
 // show more information about the id flower of the catalog
 router.get('/items/:id', validationId, showFlowerId)
 
 // update the information of the id flower of the catalog (only usable for the Admin)
-router.put('/items/:id', validationId, validationInformationUpdateFLower, updateFlower)
+router.put('/items/:id', authMiddleware, rolesMiddleware('admin'), validationId, validationInformationUpdateFLower, updateFlower)
 
 // dalete the id flower of the catalog (only usable for the Admin)
-router.delete('/items/:id', validationId, deleteFlower)
+router.delete('/items/:id', authMiddleware, rolesMiddleware('admin'), validationId, deleteFlower)
 
 // export
 module.exports = router
