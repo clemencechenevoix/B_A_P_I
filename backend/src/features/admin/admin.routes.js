ffa7daf4994606df4ab09a607b4a466f0e52ef9d
clemence chenevoix <cchenevoix@gmail.com>
2026-09-24 14:09:36 +0200

    FEAT(admin): add the role verification on the management tool (CRUD) of the family and localisation

--- backend/src/features/admin/admin.routes.js
+++ backend/src/features/admin/admin.routes.js
@@ -5,32 +5,35 @@ const router = express.Router();
 const {showFamily, createFamily, updateFamily, deleteFamily, showLocalisation, createLocalisation, updateLocalisation, deleteLocalisation} = require('./admin.controller.js')
 const {validationId, validationInformationUpdateFamily, validationInformationUpdateLocalisation, validationInformationCreateFamily, validationInformationCreateLocalisation} = require('./admin.validation.js')
 
+const {authMiddleware} = require("../../security/middleware/auth.middleware")
+const {rolesMiddleware} = require("../../security/middleware/roles.middleware")
+
 
 // implementation of the routes logic for the admin
 
 // show the information about all the family
-router.get('/family', showFamily)
+router.get('/family', authMiddleware, rolesMiddleware('admin'), showFamily)
 
 // create a new family for the flower
-router.post('/family', validationInformationCreateFamily, createFamily)
+router.post('/family', authMiddleware, rolesMiddleware('admin'), validationInformationCreateFamily, createFamily)
 
 // update the id family
-router.put('/family/:id', validationId, validationInformationUpdateFamily, updateFamily)
+router.put('/family/:id', authMiddleware, rolesMiddleware('admin'), validationId, validationInformationUpdateFamily, updateFamily)
 
 // delete the family id
-router.delete('/family/:id', validationId, deleteFamily)
+router.delete('/family/:id', authMiddleware, rolesMiddleware('admin'), validationId, deleteFamily)
 
 // show the information about all the localisation
-router.get('/localisation', showLocalisation)
+router.get('/localisation', authMiddleware, rolesMiddleware('admin'), showLocalisation)
 
 // create a new localisation for the flower
-router.post('/localisation', validationInformationCreateLocalisation, createLocalisation)
+router.post('/localisation', authMiddleware, rolesMiddleware('admin'), validationInformationCreateLocalisation, createLocalisation)
 
 // update the id localisation
-router.put('/localisation/:id', validationId, validationInformationUpdateLocalisation, updateLocalisation)
+router.put('/localisation/:id', authMiddleware, rolesMiddleware('admin'), validationId, validationInformationUpdateLocalisation, updateLocalisation)
 
 // delete the localisation id
-router.delete('/localisation/:id', validationId, deleteLocalisation)
+router.delete('/localisation/:id', authMiddleware, rolesMiddleware('admin'), validationId, deleteLocalisation)
 
 // export
 module.exports = router
