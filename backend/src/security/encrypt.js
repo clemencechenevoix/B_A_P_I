77c15bc1ed9a32d8ba917c6c1bbeb1248e1cb88a
clemence chenevoix <cchenevoix@gmail.com>
2026-09-24 14:13:00 +0200

    DOCS(security): add comments

--- backend/src/security/encrypt.js
+++ backend/src/security/encrypt.js
@@ -1,15 +1,26 @@
-// Hashing and comparing the password using bcrypt
-
+// require
 const bcrypt = require('bcrypt')
 
-// Hash the password using bcrypt with 10 rounds of hashing
+// function
+
+/**
+* @description delete the flower id
+* @param {string} password, password given
+* @return {hash} the information of the deleted flower
+*/
 const hashPassword = async (password) => {
     return await bcrypt.hash(password, 10)
 }
 
-// Compare the plain password with the hashed password
-const comparePassword = async (plain, hash) => {
-    return await bcrypt.compare(plain, hash)
+/**
+* @description compare a given password
+* @param {string} plainPassword, password given to compare with the hashPassword
+* @param {hash} hashPassword, encoded password in with bcrypt
+* @return {promise} resolved with the comparison result salt or rejected with an Error
+*/
+const comparePassword = async (plainPassword, hashPassword) => {
+    return await bcrypt.compare(plainPassword, hashPassword)
 }
 
+// exports
 module.exports = { hashPassword, comparePassword }
